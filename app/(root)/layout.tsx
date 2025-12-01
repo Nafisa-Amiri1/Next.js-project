9084977d79f0c69702d17cdf3a630315b3a6ba4e
Nafisa-Amiri1 <nafisaamiri107@gmail.com>
2025-12-01 16:51:09 +0430

    add a devicon in layouts

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,25 @@
+import LeftSidebar from '@/components/navigation/LeftSidebar';
 import Navbar from '@/components/navigation/navbar';
+import RightSidebar from '@/components/navigation/RightSidebar';
 import { ReactNode } from 'react';
 import React from 'react';
 
-const RootLayout = ({children}:{children:ReactNode}) => {
+const RootLayout = ({ children }: { children: ReactNode }) => {
     return (
-        <main>
+        <main className='bg-[#F9F8F6] dark:bg-[#000000] realtive'>
             <Navbar />
-            {children}
-         </main>
+            <div className='flex'>
+                <LeftSidebar />
+
+                <section className='flex min-h0screen flex-1 flex-col
+                px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
+                    <div className='mx-auto w-full max-w-5xl '>
+                        {children}
+                    </div>
+                </section>
+                <RightSidebar />
+            </div>
+        </main>
     );
 }
 export default RootLayout;
